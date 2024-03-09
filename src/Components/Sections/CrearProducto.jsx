979bdf86dcc992132129ca41f615f6eda80809c3
Julio Rafael Botta <jrb12494@gmail.com>
2024-03-09 00:23:52 -0300

    se comenzó con las validaciones de formik

--- src/Components/Sections/CrearProducto.jsx
+++ src/Components/Sections/CrearProducto.jsx
@@ -1,19 +1,57 @@
+//import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+//import { validarCategoria } from "../../helpers/validaciones";
+import clsx from "clsx";
+import * as Yup from "yup"
+import { useFormik } from "formik";
 
 const CrearProducto = () => {
   //los productos van a tener las siguientes props: titulo, categoría y además un indentificador único
-  const handleSubmit=(e)=>{
+  //const [title, setTitle] = useState("");
+  //const [description, setDescription] = useState("");
+  //const [category, SetCategory] = useState("");
+  const ProdcutoSchema=Yup.object().shape(
+    {
+      title: Yup.string().min(4, "min. 4 caracteres").max(20, "máx. 20 caracteres").required("el campo es requerido"),
+      description: Yup.string().min(4).max(200).required("el campo es requerido"),
+      category: Yup.string().required("el campo es requerido")
+    }
+  );
+
+  const initialValues={
+    title:'',
+    description:'',
+    category:'',
+  };
+
+  const formik=useFormik({
+    initialValues,
+    validationSchema: ProdcutoSchema,
+    validateOnBlur: true,
+    validateOnChange: true,
+    onSubmit: (values)=>{
+      console.log("values de formik->", values);
+    }
+  })
+
+ /* const handleSubmit = (e) => {
     e.preventDefault();
     console.log("desde submit");
-  }
-  
+    const nuevoProducto = {
+      titulo: title,
+      descripcion: description,
+      categoria: category,
+    };
+    console.log("nuevo producto-->", nuevoProducto);
+  };*/
+
   return (
     <div className="container py-3 my-3">
       <div className="text-center">
         <h2>Crear Producto</h2>
       </div>
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={formik.handleSubmit}>
         <Form.Group className="mb-3" controlId="title">
           <Form.Label>Título</Form.Label>
           <Form.Control
@@ -21,7 +59,25 @@ const CrearProducto = () => {
             placeholder="Ingrese el título del producto"
             minLength={4}
             maxLength={25}
+            //value={title}
+            /*onChange={(e) => {
+              setTitle(e.currentTarget.value);
+            }}*/
+            name="title"
+            {...formik.getFieldProps('title')}
+            className={clsx('form-control',{
+              'is-invalid': formik.touched.title && formik.errors.title
+            },
+            {
+              'is-valid': formik.touched.title && !formik.errors.title
+            }
+            )}
           />
+          {formik.touched.title && formik.errors.title && (
+            <div className="mt-2 text-danger fw-bolder">
+              <span role="alert">{formik.errors.title}</span>
+            </div>
+          )}
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="description">
@@ -33,17 +89,64 @@ const CrearProducto = () => {
             rows={3}
             minLength={4}
             maxLength={200}
+            //value={description}
+            //onChange={(e) => {
+            //  setDescription(e.currentTarget.value);
+            //}}
+            name="description"
+            {...formik.getFieldProps('description')}
+            className={clsx('form-control',{
+              'is-invalid': formik.touched.description && formik.errors.description
+            },
+            {
+              'is-valid': formik.touched.description && !formik.errors.description
+            }
+            )}
           />
+          {formik.touched.description && formik.errors.description && (
+            <div className="mt-2 text-danger fw-bolder">
+              <span role="alert">{formik.errors.description}</span>
+            </div>
+          )}
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="category">
-        <Form.Label>Categoría</Form.Label>
-        <Form.Select aria-label="category">
-          <option value="">Seleccione una categoría</option>
-          <option value="Bebidas">Bebidas</option>
-          <option value="Alimentos">Alimentos</option>
-          <option value="Limpieza">Limpieza</option>
-        </Form.Select>
+          <Form.Label>Categoría</Form.Label>
+          <Form.Select
+            aria-label="category"
+            /*value={category}
+            onChange={(e) => {
+              let resultado=validarCategoria(e.currentTarget.value);
+              console.log("resultado de validar", resultado);
+              SetCategory(e.currentTarget.value);
+            }} className={clsx("form-select",
+            {
+              "is-valid": validarCategoria(category)
+            },
+            {
+              "is-invalid": !validarCategoria(category)
+            }
+            )}*/
+            name="category"
+            {...formik.getFieldProps('category')}
+            className={clsx('form-control',{
+              'is-invalid': formik.touched.category && formik.errors.category
+            },
+            {
+              'is-valid': formik.touched.category && !formik.errors.category
+            }
+            )}
+          >
+            <option value="">Seleccione una categoría</option>
+            <option value="Bebidas">Bebidas</option>
+            <option value="Alimentos">Alimentos</option>
+            <option value="Limpieza">Limpieza</option>
+          </Form.Select>
+          {formik.touched.category && formik.errors.category && (
+            <div className="mt-2 text-danger fw-bolder">
+              <span role="alert">{formik.errors.category}</span>
+            </div>
+          )}
         </Form.Group>
 
         <Button variant="primary" type="submit">
