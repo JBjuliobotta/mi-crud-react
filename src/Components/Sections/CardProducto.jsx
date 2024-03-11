888162008a6cfc4d3eec80f877ac4d998cc1b180
Julio Rafael Botta <jrb12494@gmail.com>
2024-03-11 01:19:44 -0300

    se corrigió el estilo de las cards

--- src/Components/Sections/CardProducto.jsx
+++ src/Components/Sections/CardProducto.jsx
@@ -5,13 +5,13 @@ import { Col } from "react-bootstrap";
 const CardProducto = ({ producto }) => {
   return (
       <Col xs={12} md={6}>
-        <Card style={{ width: "18rem" }}>
+        <Card style={{ width: "18rem" }} className="m-3">
           <Card.Img variant="top" src="holder.js/100px180" />
           <Card.Body>
             <Card.Title>{producto.title}</Card.Title>
             <Card.Text>
-              <span className="mb-2">{producto.description}</span>
-              <span className="fs-4">{producto.category}</span>
+              <span className="mb-2 d-block">{producto.description}</span>
+              <span className="fs-4 d-block">{producto.category}</span>
             </Card.Text>
             <Button variant="primary">Ver más</Button>
           </Card.Body>
